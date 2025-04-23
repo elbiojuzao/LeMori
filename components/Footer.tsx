c095c1d4a431bbc6f8ef2b23c3917d39419d609a
Elbio Junior <elbiojuzao@gmail.com>
2025-04-23 11:53:13 -0300

    footer
    alterado padrão de cores

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
-// components/Footer.tsx
 export default function Footer() {
   return (
-    <footer className="bg-indigo-300 text-center text-white bg-gray-100 text-sm py-4 ">
+    <footer className="bg-[#c4ddd6] text-[#333] text-center text-sm py-4">
       &copy; {new Date().getFullYear()} LeMori - Lembrança e Memória. Todos os direitos reservados.
     </footer>
   )
