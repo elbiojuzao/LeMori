a06a5a983df8b7fba1e92d1d6e1790aacd302516
Elbio Junior <elbiojuzao@gmail.com>
2025-04-30 10:41:25 -0300

    itens pedido
    criação do itens pedido para armazenar itens pedidos com referencia a pedidos

---
+++ models/ItemPedido.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface ItemPedidoDocument extends Document {
+  tipoItem: 'homenagem' | 'plaquinha' | 'chaveiro'
+  produtoId: mongoose.Schema.Types.ObjectId
+  nomeProduto: string
+  quantidade: number
+  precoUnitario: number
+  homenagemId?: mongoose.Schema.Types.ObjectId
+  pedidoId: mongoose.Schema.Types.ObjectId
+}
+
+const ItemPedidoSchema = new Schema({
+  tipoItem: { type: String, enum: ['homenagem', 'plaquinha', 'chaveiro'], required: true },
+  produtoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Produto', required: true },
+  nomeProduto: { type: String, required: true },
+  quantidade: { type: Number, required: true },
+  precoUnitario: { type: Number, required: true },
+  homenagemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Homenagem' },
+  pedidoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Pedido', required: true },
+}, { timestamps: true })
+
+const ItemPedido = mongoose.models.ItemPedido || mongoose.model<ItemPedidoDocument>('ItemPedido', ItemPedidoSchema)
+export default ItemPedido
