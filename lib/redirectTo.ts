ac48e13f9089c925e3675260852780bb73eb17df
Elbio Junior <elbiojuzao@gmail.com>
2025-05-16 15:15:01 -0300

    redirecionamento para perfil
    alterado redirecionamento após login
    adicionado header e footer em login e registro

--- lib/redirectTo.ts
+++ lib/redirectTo.ts
@@ -1,12 +1,12 @@
 import { NextRouter } from 'next/router'
 
-export function redirectToAfterLogin(router: NextRouter, fallback: string = '/dashboard') {
+export function redirectToAfterLogin(router: NextRouter, fallback: string = '/perfil') {
   const redirect = router.query.redirect
   const destino = typeof redirect === 'string' ? redirect : fallback
   router.push(destino)
 }
 
-export function replaceToAfterLogin(router: NextRouter, fallback: string = '/dashboard') {
+export function replaceToAfterLogin(router: NextRouter, fallback: string = '/perfil') {
   const redirect = router.query.redirect
   const destino = typeof redirect === 'string' ? redirect : fallback
   router.replace(destino)
